b097e51cd1cb90e1a9e7baab23ef3698e364971e
Cristina Pena <cristinalpena@gmail.com>
2020-02-18 15:20:07 -0700

    Refactor calculate cost method to use reduce and find, no more if statement

--- src/recipe.js
+++ src/recipe.js
@@ -10,17 +10,14 @@ class Recipe {
   }
 
   calculateCost() {
-    let costCounter = 0;
-    this.ingredients.forEach(ingredient => {
-      this.ingredientsData.find(specificIngredient => {
-        if (specificIngredient.id === ingredient.id) {
-          costCounter += (Number(specificIngredient.estimatedCostInCents) *
-          Number(ingredient.quantity.amount))
-        }
-      })
-    });
-    let costInDollars = (costCounter/100).toFixed(2);
-    return Number(costInDollars);
+    return this.ingredients.reduce((sum, ingredient) => {
+      let matchingIngredient = this.ingredientsData.find(specificIngredient => {
+        return specificIngredient.id === ingredient.id;
+      });
+      let dollars = (((matchingIngredient.estimatedCostInCents) * (ingredient.quantity.amount))/100)
+      sum += dollars;
+      return sum;
+    }, 0)
   }
 
   filterRecipesByTag(selectedTag) {
