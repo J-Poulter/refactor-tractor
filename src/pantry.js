4b8cce399f59edeb2dbe012b39bf0a72e0d55b2e
Jeremy Poulter <Jpoulter@gmail.com>
2020-02-18 19:26:32 -0700

    Add code for the first few methods in pantry class

--- src/pantry.js
+++ src/pantry.js
@@ -1,33 +1,65 @@
 class Pantry {
-  constructor(userIngredients) {
-    this.contents = userIngredients
+  constructor(userIngredients, ingredientsData) {
+    this.contents = userIngredients,
+    this.ingredientsData = ingredientsData
   }
 
-  evaluateIfEnoughIngredients(chosenRecipe, ingredientsData) {
-    // chosenRecipe.ingredients.forEach(ingredient => {
-    //   this.contents.find(pantryIngredient => {
-    //     pantryIngredient.ingredient === ingredient.id
-      // })
-      let ingredientDetails = chosenRecipe.ingredients.map(ingredient => {
-        return {
-          name: ingredient.name,
-          id: ingredient.id,
-          qty: ingredient.quantity.amount,
-          cost: ingredientsData.find(ingredientInfo => {
-            return ingredientInfo.id === ingredient.id
-          }).estimatedCostInCents
-        }
-      })
-      
-    }
+  compareRecipeToPantryIngredients(chosenRecipe) {
+
+    let ingredientDetails = chosenRecipe.ingredients.map(ingredient => {
+      return {
+        name: ingredient.name,
+        id: ingredient.id,
+        qty: ingredient.quantity.amount,
+        unit: ingredient.quantity.unit,
+        cost: this.ingredientsData.find(ingredientInfo => {
+          return ingredientInfo.id === ingredient.id
+        }).estimatedCostInCents
+      };
+    });
+
+    ingredientDetails.forEach(ingredient => {
+      let pantryMatch = this.contents.find(item => {
+        if (ingredient.id === item.ingredient) {
+          ingredient.qty -= item.amount
+        };
+      });
+    });
+
+    let ingredientsNeeded = [];
+    ingredientDetails.forEach(ingredient => {
+      if (ingredient.qty > 0) {
+        ingredientsNeeded.push(ingredient)
+      };
+    });
+    return ingredientsNeeded;
+  }
+
+  evaluateIfEnoughIngredients(chosenRecipe) {
+    let ingredientsNeeded = this.compareRecipeToPantryIngredients(chosenRecipe);
+    return (ingredientsNeeded.length > 0) ? false : true;
+  }
 
 
   determineAdditionalNeededIngredients(chosenRecipe) {
-
+    let ingredientsNeeded = this.compareRecipeToPantryIngredients(chosenRecipe);
+    let neededQuantity = ingredientsNeeded.map(ingredient => {
+      return `${ingredient.name} ${ingredient.qty} ${ingredient.unit}`
+    })
+    return neededQuantity
   }
 
   calculateCostOfAdditionalIngredients(chosenRecipe) {
-
+    let ingredientsNeeded = this.compareRecipeToPantryIngredients(chosenRecipe);
+    let additionalIngredientCost = ingredientsNeeded.map(ingredient => {
+      return ingredient.name + ' $' + (ingredient.qty * ingredient.cost)/100
+    })
+    let totalCost = ingredientsNeeded.reduce((cost, cur) => {
+      cost += (cur.qty * cur.cost)/100
+      return cost
+    }, 0).toFixed(2)
+    additionalIngredientCost.push('total $' + totalCost)
+    return additionalIngredientCost
   }
 
   updatePantryContent() {
